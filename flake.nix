59bd5cd05e56e907f9a1fd8c0ea0d6bc8229c765
Martin Brignall <martin.brignall@OVO-LPXDY5HKQ2.local>
2026-03-26 14:33:13 +0000

    Tidy up Flake

--- flake.nix
+++ flake.nix
@@ -11,16 +11,16 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
     darwinConfigurations."OVO-LPXDY5HKQ2" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin"; # Set to your system architecture
+      system = "aarch64-darwin";
       modules = [
-        ./darwin-configuration.nix # Your external Darwin configuration
+        ./darwin-configuration.nix
         { users.users."martin.brignall".home = "/Users/martin.brignall"; }
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users."martin.brignall" =
-            import ./home.nix; # Your external Home Manager configuration
+            import ./home.nix;
         }
       ];
     };
